9bd511d1edd7e8873282f7d16695ccd8917ae683
willshuffy <saputrawilly26@gmail.com>
2020-11-10 08:13:44 +0700

    Run One Time Task

--- app/src/main/java/com/willshuffyproject/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/myworkmanager/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.willshuffyproject.myworkmanager
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.work.*
+import androidx.work.Data.Builder
+import androidx.work.WorkInfo.State
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnOneTimeTask -> startOneTimeTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        textStatus.text = getString(R.string.status)
+        val data = Builder()
+            .putString(MyWorker.EXTRA_CITY, editCity.text.toString())
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        WorkManager.getInstance().enqueue(oneTimeWorkRequest)
+        WorkManager.getInstance().getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this@MainActivity, object : Observer<WorkInfo> {
+            override fun onChanged(workInfo: WorkInfo) {
+                val status = workInfo.state.name
+                textStatus.append("\n" + status)
+            }
+        })
     }
 }
